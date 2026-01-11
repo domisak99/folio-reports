39f4d62f8657fccb40aa73c35bfbca61d892ea58
Dominik Palán <domik.palan@seznam.cz>
2026-01-11 22:54:37 +0100

    added count payments2

---
+++ reports/count_payments2.sql
@@ -0,0 +1,23 @@
+--metadb:function count_payments2
+
+DROP FUNCTION IF EXISTS count_payments2;
+
+CREATE FUNCTION count_payments(
+    start_date date DEFAULT '1000-01-01',
+    end_date date DEFAULT '3000-01-01')
+RETURNS TABLE(
+    user_id uuid,
+    payment_count bigint,
+    total_amount numeric(19,2))
+AS $$
+SELECT user_id,
+       count(*) AS payment_count,
+       sum(amount_action) AS total_amount
+    FROM folio_feesfines.feefineactions__
+    WHERE type_action = 'Payment'
+      AND start_date <= date_action AND date_action < end_date
+    GROUP BY user_id
+$$
+LANGUAGE SQL
+STABLE
+PARALLEL SAFE;
