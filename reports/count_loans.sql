b866c75103c45f3d56920e2a1e0808e077b136e5
Dominik Palán <domik.palan@seznam.cz>
2026-01-11 23:07:04 +0100

    added jsonb extraction

--- reports/count_loans.sql
+++ reports/count_loans.sql
@@ -6,14 +6,15 @@ CREATE FUNCTION count_loans(
     start_date date DEFAULT '1000-01-01',
     end_date date DEFAULT '3000-01-01')
 RETURNS TABLE(
-    itemId uuid,
+    item_id uuid,
     loan_count bigint)
 AS $$
-SELECT itemId::uuid,
+SELECT jsonb_extract_path_text(jsonb, 'itemId')::uuid AS item_id,
        count(*) AS loan_count
-    FROM folio_circulation.loan__t
-    WHERE start_date <= loan_date::date AND loan_date::date < end_date
-    GROUP BY itemId
+    FROM folio_circulation.loan
+    WHERE start_date <= (jsonb_extract_path_text(jsonb, 'loanDate')::timestamptz)::date 
+      AND (jsonb_extract_path_text(jsonb, 'loanDate')::timestamptz)::date < end_date
+    GROUP BY jsonb_extract_path_text(jsonb, 'itemId')
 $$
 LANGUAGE SQL
 STABLE
