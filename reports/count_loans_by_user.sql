94392f3143cc81bb4aa7f01b8173b4c39b0a8a0f
Dominik Palán <domik.palan@seznam.cz>
2026-01-12 10:08:51 +0100

    cahnge username test

--- reports/count_loans_by_user.sql
+++ reports/count_loans_by_user.sql
@@ -6,21 +6,21 @@ CREATE FUNCTION count_loans_by_user(
     start_date date DEFAULT '1000-01-01',
     end_date date DEFAULT '3000-01-01')
 RETURNS TABLE(
-    "ID uživatele" uuid,
+    id_uzivatele uuid,
     "Uživatelské jméno" text,
-    "Jméno" text,
-    "Příjmení" text,
-    "Email" text,
-    "Skupina čtenářů" text,
+    jmeno text,
+    prijmeni text,
+    email text,
+    skupina_ctenaru text,
     pocet_vypujcek bigint)
 AS $$
 SELECT 
-    jsonb_extract_path_text(l.jsonb, 'userId')::uuid AS "ID uživatele",
+    jsonb_extract_path_text(l.jsonb, 'userId')::uuid AS id_uzivatele,
     jsonb_extract_path_text(u.jsonb, 'username') AS "Uživatelské jméno",
-    jsonb_extract_path_text(u.jsonb, 'personal', 'firstName') AS "Jméno",
-    jsonb_extract_path_text(u.jsonb, 'personal', 'lastName') AS "Příjmení",
-    jsonb_extract_path_text(u.jsonb, 'personal', 'email') AS "Email",
-    jsonb_extract_path_text(u.jsonb, 'patronGroup') AS "Skupina čtenářů",
+    jsonb_extract_path_text(u.jsonb, 'personal', 'firstName') AS jmeno,
+    jsonb_extract_path_text(u.jsonb, 'personal', 'lastName') AS prijmeni,
+    jsonb_extract_path_text(u.jsonb, 'personal', 'email') AS email,
+    jsonb_extract_path_text(u.jsonb, 'patronGroup') AS skupina_ctenaru,
     count(*) AS pocet_vypujcek
 FROM folio_circulation.loan l
 LEFT JOIN folio_users.users u 
